8f8f9c1fb0ab46dfd83080efb266c3923e6f08bb
WenDrakEZ <50702757+WenDrakEZ@users.noreply.github.com>
2022-04-28 15:35:45 +0700

    Add files via upload

---
+++ Behav2/WeightStrategy.java
@@ -0,0 +1,13 @@
+
+import java.util.Comparator;
+import java.util.List;
+
+public class WeightStrategy implements SortStrategy {
+    // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-
+
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparing(Person::getWeight));
+    }
+
+}
